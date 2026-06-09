536b2ddad36f8d976be2b57d4cd720a8645514f2
Piyush-dross <kumarsinghpiyush62@gmail.com>
2026-06-09 13:23:43 +0530

    Add selection sort implementation

---
+++ sorting/selectionsort.java
@@ -0,0 +1,26 @@
+public class selectionsort {
+    public static void selectionsort(int arr[]){
+        int n=arr.length;
+        for(int i=0;i<n-1;i++){
+            int minPos=i;
+            for(int j=i+1;j<n-1;j++){
+                if(arr[minPos]>arr[j]){
+                    minPos=j;
+                }
+            }
+            int temp=arr[minPos];
+            arr[minPos]=arr[i];
+            arr[i]=temp;
+        }
+    }  public static void main(String[] args) {
+        int arr[]={5,4,1,3,2};
+        selectionsort(arr);
+        for(int nums:arr){
+            System.out.println(nums);
+        }
+    }
+
+
+
+
+}
