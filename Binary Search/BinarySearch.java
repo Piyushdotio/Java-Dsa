c8e628fe93ccdf1f6e34273e23cf2598e60c076e
Piyush-dross <kumarsinghpiyush62@gmail.com>
2026-06-19 07:58:38 +0530

    binarysearch

--- Binary Search/BinarySearch.java
+++ Binary Search/BinarySearch.java
@@ -1,14 +1,13 @@
 
 
-
-public class upperrBound {
+public class BinarySearch {
 
     public static void optimal(int arr[],int x){
         int low=0,high=arr.length-1;
         int lb=arr.length;
         while(low<=high){
             int mid=(low+high)/2;
-            if(arr[mid]>x){
+            if(arr[mid]>=x){
                 lb=mid;
                 high=mid-1;
             }
@@ -20,8 +19,7 @@ public class upperrBound {
     }
         public static void main(String[] args) {
             int arr[]={1,2,3,3,5,8,8,10,10,11};
-            optimal(arr,6);
+            optimal(arr,10);
 
         }
 }
-
