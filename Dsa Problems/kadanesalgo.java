162eff955c9ec53a1f281008869c04783576a9fd
Piyush-dross <kumarsinghpiyush62@gmail.com>
2026-06-24 10:11:01 +0530

    Implement Kadane's algorithm for maximum subarray sum and refactor main method for clarity

--- Dsa Problems/kadanesalgo.java
+++ Dsa Problems/kadanesalgo.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+
 public class kadanesalgo {
     public static int brute(int arr[]){
         int n=arr.length;
@@ -12,10 +14,40 @@ public class kadanesalgo {
         }
         return maxsum;
     }
+    
+    public static int[] optimal(int arr[]){//kadanes algo o(n)
+        int n=arr.length;
+        int currsum=0;
+        int start=0;
+        int ansstart=-1;
+        int end=-1;
+        int maxsum=Integer.MIN_VALUE;
+        for(int i = 0; i < n; i++){
+            if(currsum==0){
+                start=i;
+            }
+            currsum+=arr[i];
+            if(currsum>maxsum){
+                maxsum=currsum;
+                ansstart=start;
+                end=i;
+            }
+            if(currsum<0){
+                currsum=0;
+            }
+            
+        }
+        
+        return new int[]{ansstart, end};
+    }
     public static void main(String[] args) {
         int arr[]={2, 3, 5, -2, 7, -4};
-        System.out.println(brute(arr));
-
+        int n[]=optimal(arr);
+        int start=n[0];
+        int end=n[1];
+        for(int i = start; i <= end; i++){
+            System.out.print(arr[i]+" ");
+        }
 
     }
 }
--- Dsa Problems/tempCodeRunnerFile.java
+++ Dsa Problems/tempCodeRunnerFile.java
@@ -1,35 +1,3 @@
-  public static void unionopt(int arr[],int arr2[]){
-        int n=arr.length;
-        int m=arr2.length;
-        int i=0;
-        int j=0;
-        ArrayList<Integer> list=new ArrayList<>();
-        while(i < n&&j < m){
-            if(arr[i]<=arr2[j]){
-                if(list.isEmpty()||list.get(list.size()-1)!=arr[i]){
-                list.add(arr[i]);
+ if(currsum==0){
+                start=i;
             }
-            i++;
-            }
-            
-            else{
-            if(list.isEmpty()||list.get(list.size()-1)!=arr2[j]){
-                list.add(arr2[j]);
-            }
-            j++;
-        }
-        }
-        while(i<n){
-            if(list.isEmpty()||list.get(list.size()-1)!=arr[i]){
-                list.add(arr[i]);
-            }
-            i++;
-        }
-        while(j<m){
-            if(list.isEmpty()||list.get(list.size()-1)!=arr2[j]){
-                list.add(arr2[j]);
-            }
-            j++;
-        }
-        System.out.print(list);
-    }
