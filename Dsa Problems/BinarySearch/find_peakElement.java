7eaa7fefb354e0086fa49de2f497145857347f69
Piyush-dross <kumarsinghpiyush62@gmail.com>
2026-07-09 13:14:12 +0530

    Refactor peakbrute method to improve variable naming and logic clarity

--- Dsa Problems/BinarySearch/find_peakElement.java
+++ Dsa Problems/BinarySearch/find_peakElement.java
@@ -4,16 +4,14 @@ public class find_peakElement {
     public static int peakbrute(int arr[]){
         int n=arr.length;
         int max=Integer.MIN_VALUE;
-        int pos=-1;
+        int index=0;
         for(int i = 0; i < n; i++){
-
-
             if(max<arr[i]){
                 max=arr[i];
-                pos++;
+                index=i;
             }
         }
-        return pos;
+        return index;
     }
     public static void main(String[] args) {
         int arr[]={1,2,3,1};
